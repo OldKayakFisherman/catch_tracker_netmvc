4890b2779f06249090b175b9a92f4bb660612f39
Rick Flagg <richard.lee.flagg@outlook.com>
2024-05-21 15:04:44 -0400

    updating unit tests

---
+++ CatchTrackerNetMVC.Web.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CatchTrackerNetMVC.Web.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

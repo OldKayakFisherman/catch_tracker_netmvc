b738c3794fd73792810a3c7e4f3f113290e651d2
Rick Flagg <richard.lee.flagg@outlook.com>
2024-05-27 19:36:07 -0400

    Updating repositories

--- CatchTrackerNetMVC.Web.Tests/Data/Respository/CatchRepositoryTests.cs
+++ CatchTrackerNetMVC.Web.Tests/Data/Respository/CatchRepositoryTests.cs
@@ -16,26 +16,152 @@ public class CatchRepositoryTests
             {
                 //Instantiate our repository
                 CatchRepository repo = new CatchRepository(ctx);
-                
+
                 //Create some test records
                 IList<CatchDetail> catchRecords = TestDataHelper.CreateTestCatchDetails(100);
-                
-                repo.BulkInsert(catchRecords);
 
-                IList<string> uniqueTechniques = repo.GetUniqueTechniques();
-                
-                
-                Assert.IsNotNull(uniqueTechniques);
-                Assert.Greater(uniqueTechniques.Count, 0);
-                Assert.LessOrEqual(uniqueTechniques.Count, 3);
-                
-                
-                
-                
-                
-                
+                repo.BulkAdd(catchRecords);
+
+                IList<string> uniqueRecords = repo.GetUniqueTechniques();
+
+
+                Assert.IsNotNull(uniqueRecords);
+                Assert.Greater(uniqueRecords.Count, 0);
+                Assert.LessOrEqual(uniqueRecords.Count, 3);
+
             }
         }
-        
     }
+    
+    [TestCase]
+    public void TestGetUniqueTerminalTackle()
+    {
+        using (var factory = new TestApplicationDbContextFactory())
+        {
+            using (var ctx = factory.CreateContext())
+            {
+                //Instantiate our repository
+                CatchRepository repo = new CatchRepository(ctx);
+
+                //Create some test records
+                IList<CatchDetail> catchRecords = TestDataHelper.CreateTestCatchDetails(100);
+
+                repo.BulkAdd(catchRecords);
+
+                IList<string> uniqueRecords = repo.GetUniqueTerminalTackle();
+
+
+                Assert.IsNotNull(uniqueRecords);
+                Assert.Greater(uniqueRecords.Count, 0);
+                Assert.LessOrEqual(uniqueRecords.Count, 2);
+
+            }
+        }
+    }
+
+    [TestCase]
+    public void TestGetUniqueSpecies()
+    {
+        using (var factory = new TestApplicationDbContextFactory())
+        {
+            using (var ctx = factory.CreateContext())
+            {
+                //Instantiate our repository
+                CatchRepository repo = new CatchRepository(ctx);
+
+                //Create some test records
+                IList<CatchDetail> catchRecords = TestDataHelper.CreateTestCatchDetails(100);
+
+                repo.BulkAdd(catchRecords);
+
+                IList<string> uniqueRecords = repo.GetUniqueSpecies();
+
+
+                Assert.IsNotNull(uniqueRecords);
+                Assert.Greater(uniqueRecords.Count, 0);
+                Assert.LessOrEqual(uniqueRecords.Count, 5);
+
+            }
+        }
+    }
+
+    [TestCase]
+    public void TestGetUniqueSkyConditions()
+    {
+        using (var factory = new TestApplicationDbContextFactory())
+        {
+            using (var ctx = factory.CreateContext())
+            {
+                //Instantiate our repository
+                CatchRepository repo = new CatchRepository(ctx);
+
+                //Create some test records
+                IList<CatchDetail> catchRecords = TestDataHelper.CreateTestCatchDetails(100);
+
+                repo.BulkAdd(catchRecords);
+
+                IList<string> uniqueRecords = repo.GetUniqueSkyConditions();
+
+
+                Assert.IsNotNull(uniqueRecords);
+                Assert.Greater(uniqueRecords.Count, 0);
+                Assert.LessOrEqual(uniqueRecords.Count, 4);
+
+            }
+        }
+    }
+
+    [TestCase]
+    public void TestGetUniqueBaits()
+    {
+        using (var factory = new TestApplicationDbContextFactory())
+        {
+            using (var ctx = factory.CreateContext())
+            {
+                //Instantiate our repository
+                CatchRepository repo = new CatchRepository(ctx);
+
+                //Create some test records
+                IList<CatchDetail> catchRecords = TestDataHelper.CreateTestCatchDetails(100);
+
+                repo.BulkAdd(catchRecords);
+
+                IList<string> uniqueRecords = repo.GetUniqueBaits();
+
+
+                Assert.IsNotNull(uniqueRecords);
+                Assert.Greater(uniqueRecords.Count, 0);
+                Assert.LessOrEqual(uniqueRecords.Count, 4);
+
+            }
+        }
+    }
+
+    [TestCase]
+    public void TestGetUniqueRods()
+    {
+        using (var factory = new TestApplicationDbContextFactory())
+        {
+            using (var ctx = factory.CreateContext())
+            {
+                //Instantiate our repository
+                CatchRepository repo = new CatchRepository(ctx);
+
+                //Create some test records
+                IList<CatchDetail> catchRecords = TestDataHelper.CreateTestCatchDetails(100);
+
+                repo.BulkAdd(catchRecords);
+
+                IList<string> uniqueRecords = repo.GetUniqueRods();
+
+
+                Assert.IsNotNull(uniqueRecords);
+                Assert.Greater(uniqueRecords.Count, 0);
+                Assert.LessOrEqual(uniqueRecords.Count, 2);
+
+            }
+        }
+    }
+
+
 }
--- CatchTrackerNetMVC.Web/Data/Repositories/CatchRepository.cs
+++ CatchTrackerNetMVC.Web/Data/Repositories/CatchRepository.cs
@@ -15,10 +15,18 @@ public class CatchRepository
         _ctx = ctx;
     }
 
-    public void BulkInsert(IList<CatchDetail> catches)
+    public void BulkAdd(IList<CatchDetail> catches)
     {
         _ctx.BulkInsert<CatchDetail>(catches);
+        _ctx.SaveChanges();
     }
+
+    public void Add(CatchDetail catchDetail)
+    {
+        _ctx.Add<CatchDetail>(catchDetail);
+        _ctx.SaveChanges();
+    }
+    
     
     public IList<string> GetUniqueTechniques()
     {
@@ -28,4 +36,50 @@ public class CatchRepository
             .Distinct()!
             .ToList<string>();
     }
+    
+    public IList<string> GetUniqueTerminalTackle()
+    {
+        return _ctx.CatchDetails
+            .Where(c => c.Technique != null)
+            .Select(t => t.Technique)
+            .Distinct()!
+            .ToList<string>();
+    }
+    
+    public IList<string> GetUniqueSpecies()
+    {
+        return _ctx.CatchDetails
+            .Where(c => true)
+            .Select(t => t.Species)
+            .Distinct()
+            .ToList<string>();
+    }
+    
+    public IList<string> GetUniqueSkyConditions()
+    {
+        return _ctx.CatchDetails
+            .Where(c => c.SkyConditions != null)
+            .Select(t => t.SkyConditions)
+            .Distinct()!
+            .ToList<string>();
+    }
+    
+    public IList<string> GetUniqueBaits()
+    {
+        return _ctx.CatchDetails
+            .Where(c => c.Bait != null)
+            .Select(t => t.Bait)
+            .Distinct()!
+            .ToList<string>();
+    }
+    
+    public IList<string> GetUniqueRods()
+    {
+        return _ctx.CatchDetails
+            .Where(c => c.Rod != null)
+            .Select(t => t.Rod)
+            .Distinct()!
+            .ToList<string>();
+    }
+    
 }
